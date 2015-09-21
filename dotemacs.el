96fa2a06fa6c21db987053e6cc991ee59d338b39
David Vazquez <Dvazquezpua@deloitte.nl>
2015-09-21 09:59:25 +0200

    Basic emacs customizations: remove UI features, enable ido...

--- dotemacs.el
+++ dotemacs.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2015  David Vazquez
 
 ;; Author: David Vazquez <davazp@debian>
-;; Keywords: 
+;; Keywords:
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -20,23 +20,57 @@
 
 ;;; Commentary:
 
-;; 
+;;
 
 ;;; Code:
 
-;;   ,           , 
-;;  /             \ 
-;; ((__-^^-,-^^-__)) 
-;;  `-_---' `---_-' 
-;;   `--|o` 'o|--' 
-;;      \  `  / 
-;;       ): :( 
-;;       :o_o: 
-;;        "-" 
+;;   ,           ,
+;;  /             \
+;; ((__-^^-,-^^-__))
+;;  `-_---' `---_-'
+;;   `--|o` 'o|--'
+;;      \  `  /
+;;       ): :(
+;;       :o_o:
+;;        "-"
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
+
+
+;;;; General settings
+
+;;; General customizations to the UI. We want to maximize the space
+;;; available for the text.
+
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+(display-time)
+
+;;; Do not generate backup files, we have git nowadays.
+(setq make-backup-files nil)
+
+
+;;; Use ido to find buffers and files. In addition of that, we define
+;;; the `switch-to-other-buffer' function to explote the locality of
+;;; references: I work most of the time with one or two buffers at the
+;;; same time.
+
+(ido-mode 1)
+(setq ido-use-filename-at-point 'guess)
+(setq read-file-name-completion-ignore-case t)
+
+(defun switch-to-other-buffer ()
+  "Switch to the most recently visited buffer. Calling this
+command repeatly will switch between the last two most recent
+buffers."
+  (interactive)
+  (switch-to-buffer (other-buffer)))
+
+(define-key global-map (kbd "C-;") 'switch-to-other-buffer)
 
 
 
