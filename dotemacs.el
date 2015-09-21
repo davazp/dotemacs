3cb4be3219e88aaaaf6ae59a243c43d441bd0728
David Vazquez <Dvazquezpua@deloitte.nl>
2015-09-21 09:45:36 +0200

    Add empty dotemacs.el file

---
+++ dotemacs.el
@@ -0,0 +1,45 @@
+;;; dotemacs.el --- My personal GNU/Emacs customizations  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2015  David Vazquez
+
+;; Author: David Vazquez <davazp@debian>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+;;   ,           , 
+;;  /             \ 
+;; ((__-^^-,-^^-__)) 
+;;  `-_---' `---_-' 
+;;   `--|o` 'o|--' 
+;;      \  `  / 
+;;       ): :( 
+;;       :o_o: 
+;;        "-" 
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+
+
+
+
+(provide 'dotemacs)
+;;; dotemacs.el ends here
