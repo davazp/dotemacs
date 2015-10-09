5d446222716fc66a1196e63024393687176ee0c6
David Vazquez <davazp@gmail.com>
2015-10-09 19:13:49 +0200

    Fix: require js2-mode

--- dotemacs.el
+++ dotemacs.el
@@ -236,6 +236,8 @@ buffers."
 (unless (package-installed-p 'js2-mode)
   (package-install 'js2-mode))
 
+(require 'js2-mode)
+
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (setq-default js2-basic-offset 2)
 (setq-default js-indent-level 2)
