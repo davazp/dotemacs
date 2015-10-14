ffc04af70df47c68987fb66dd8499cc14d318647
David Vazquez <davazp@gmail.com>
2015-10-14 20:17:05 +0200

    Better hippie-expand configuration

--- dotemacs.el
+++ dotemacs.el
@@ -60,6 +60,11 @@
 ;; but use hippie-expand instead of raw dabbrev-expand!
 (define-key global-map (kbd "M-/") 'hippie-expand)
 
+(setq hippie-expand-try-functions-list
+      '(try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill
+                           try-complete-file-name-partially try-complete-file-name
+                           try-expand-all-abbrevs try-expand-list try-expand-line
+                           try-complete-lisp-symbol-partially try-complete-lisp-symbol))
 
 ;;; Don't use tabs
 (setq-default indent-tabs-mode nil)
