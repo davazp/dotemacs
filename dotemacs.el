844651e7a70a3d99a9708dc37c4f0698af8d2ce5
David Vazquez <davazp@gmail.com>
2015-10-17 13:13:41 +0200

    Diminish elisp-slime-nav

--- dotemacs.el
+++ dotemacs.el
@@ -294,6 +294,7 @@ buffers."
 
 ;; Nice slime-like navigation for Emacs lisp with M-. and M-,
 (use-package elisp-slime-nav
+  :diminish elisp-slime-nav-mode
   :config
   (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
     (add-hook hook 'turn-on-elisp-slime-nav-mode)))
